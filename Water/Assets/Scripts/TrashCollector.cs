2851129159c142eb8abde3c90f70ab3a365b6c43
Matias Gultig <magontek@gmail.com>
2020-10-25 19:31:44 -0300

    movi los scripts a la carpeta script

--- Water/Assets/Scripts/TrashCollector.cs
+++ Water/Assets/Scripts/TrashCollector.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Policy;
 using UnityEngine;
 
 
 public class TrashCollector : MonoBehaviour
 {
+    public bool has_floater=false;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if(gameObject.GetComponent<FixedJoint>())
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
             if (hit.collider != null)
             {
                 hit.collider.gameObject.transform.SetParent(gameObject.transform);
                 hit.collider.gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0.5f, 0);
+                hit.collider.gameObject.GetComponent<FixedJoint>().
             }
         }
     }
