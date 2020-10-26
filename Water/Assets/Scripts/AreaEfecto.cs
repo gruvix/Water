c16e01710acdc3878a4d58f054abe47285f0ca4d
Matias Gultig <magontek@gmail.com>
2020-10-25 21:24:05 -0300

    indicador de area de efecto

---
+++ Water/Assets/Scripts/AreaEfecto.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class AreaEfecto : MonoBehaviour
+{
+    [Range(0, 50)]
+    public int segments = 20;
+    [Range(0, 5)]
+    public float xradius = 3;
+    [Range(0, 5)]
+    public float yradius = 3;
+    LineRenderer line;
+
+    [Range(0, 5)]
+    public float xoffset = 0;
+    [Range(0, 5)]
+    public float yoffset = 1;
+
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.SetVertexCount(segments + 1);
+        line.useWorldSpace = false;
+        CreatePoints();
+    }
+
+    void CreatePoints()
+    {
+        float x;
+        float y;
+        float z;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = xoffset + Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+            y = yoffset + Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
+
+            line.SetPosition(i, new Vector3(x, y, 0));
+
+            angle += (360f / segments);
+        }
+    }
+}
--- Water/Assets/Scripts/TrashCollector.cs
+++ Water/Assets/Scripts/TrashCollector.cs
@@ -6,7 +6,17 @@ using UnityEngine;
 public class TrashCollector : MonoBehaviour
 {
     public bool has_floater=false;
+    public float alcance = 1;
     private GameObject floater;
+    private GameObject Bote;
+    private LineRenderer AreaEfecto;
+    private void Start()
+    {
+        Bote = GameObject.Find("Bote");
+        AreaEfecto = gameObject.GetComponent<LineRenderer>();
+        Debug.Log(AreaEfecto);
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -14,18 +24,30 @@ public class TrashCollector : MonoBehaviour
             Vector2 m_puntero = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(m_puntero, Vector2.zero);
             if (has_floater){
-
+                // Si tiene algo agarrado y haces click lo deja donde hiciste click y se lo da al bote
+                floater.transform.position = m_puntero;
+                floater.transform.SetParent(Bote.transform);
+                floater.GetComponent<FixedJoint2D>().connectedBody = Bote.GetComponent<Rigidbody2D>();
+                Debug.Log(floater.GetComponent<FixedJoint2D>().connectedBody);
+                has_floater = false;
+                AreaEfecto.enabled = false;
             }
             else{
                 if (hit.collider != null)
                 {
-                    hit.collider.gameObject.transform.SetParent(gameObject.transform);
-                    hit.collider.gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0.3f, 0);
-                    
-                    hit.collider.gameObject.GetComponent<FixedJoint2D>().connectedBody=gameObject.GetComponent<Rigidbody2D>();
-                    hit.collider.gameObject.GetComponent<FixedJoint2D>().enabled = true;
+                    floater = hit.collider.gameObject;
+                    floater.transform.SetParent(gameObject.transform);
+                    floater.transform.position = gameObject.transform.position + new Vector3(0, 0.3f, 0);
+                    floater.GetComponent<FixedJoint2D>().connectedBody=gameObject.GetComponent<Rigidbody2D>();
+                    floater.GetComponent<FixedJoint2D>().enabled = true;
+                    has_floater = true;
+                    AreaEfecto.enabled = true;
                 }
             }
+        }
+        if (has_floater)
+        {
+
         }
     }
 }
