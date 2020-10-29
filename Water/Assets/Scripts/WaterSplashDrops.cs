fd60ae8e9b7ddda56307f960027e5ba30da0161a
Drupus <timmy.burton@hotmail.com>
2020-10-29 16:22:08 -0300

    Ultimate Water Splash Effect

--- Water/Assets/Scripts/WaterSplashDrops.cs
+++ Water/Assets/Scripts/WaterSplashDrops.cs
@@ -6,24 +6,26 @@ public class WaterSplashDrops : MonoBehaviour
 {
 
 	public ParticleSystem Drops;
+	public GameObject killer;
+	private Rigidbody2D body;
+	[Range(2f, 30f)]
+	public float Distance = 10;
 
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag != "WaterBox")
+    	body = col.GetComponent<Rigidbody2D>();
+        if(col.tag != "WaterBox" && Mathf.Abs(body.velocity.y) > 0.5f)
         {
             Debug.Log("Splash Detected");
         	var Particle = Instantiate(Drops, col.transform.position, transform.rotation);
+        	var main =  Particle.main;
+        	var collider = Particle.collision;
+        	collider.SetPlane(0, killer.transform);
+        	main.startSpeed = Mathf.Abs(body.velocity.y)*Distance;
         	Particle.enableEmission = true;
         	Particle.Play();
-        	Destroy(Particle.gameObject, 5f);
+        	Destroy(Particle.gameObject, 3f);
         }
     }
-
-
-/*
-    public void OnTriggerEnter()
-    {
-        var effect = Instantiate(Drops, tra)
-    }*/
 }
