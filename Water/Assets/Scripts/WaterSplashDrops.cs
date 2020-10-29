8066f24d8fd146d73ceac600d4a17778e25064d5
Drupus <timmy.burton@hotmail.com>
2020-10-29 11:57:24 -0300

    WaterDrops (notworking)

---
+++ Water/Assets/Scripts/WaterSplashDrops.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSplashDrops : MonoBehaviour
+{
+
+	public ParticleSystem Drops;
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag != "WaterBox")
+        {
+            Debug.Log("Splash Detected");
+        	var Particle = Instantiate(Drops, col.transform.position, transform.rotation);
+        	Particle.enableEmission = true;
+        	Particle.Play();
+        	Destroy(Particle.gameObject, 5f);
+        }
+    }
+
+
+/*
+    public void OnTriggerEnter()
+    {
+        var effect = Instantiate(Drops, tra)
+    }*/
+}
