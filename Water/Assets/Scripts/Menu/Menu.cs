78a21cd2cbc364597df708a105a80072f7f35e0f
Drupus <timmy.burton@hotmail.com>
2020-11-18 14:50:57 -0300

    Exit v2

--- Water/Assets/Scripts/Menu/Menu.cs
+++ Water/Assets/Scripts/Menu/Menu.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 
 public class Menu : MonoBehaviour
 {
+    public Button bnQuit;
     public GameObject MultiplayerMenuManager;
 
     void Start()
     {
         MultiplayerMenuManager.SetActive(true);
+        bnQuit.onClick.AddListener(QuitGame);
     }
 
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
 }
