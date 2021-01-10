a1560a88e351f6cf9823a5f80b57bab8ed286fa2
Drupus <timmy.burton@hotmail.com>
2021-01-10 15:37:19 -0300

    Nucleo Cargado

--- Water/Assets/Scripts/Starter.cs
+++ Water/Assets/Scripts/Starter.cs
@@ -7,6 +7,7 @@ public class Starter : MonoBehaviour
 {
     void Start()
     {
+        FindObjectOfType<NetworkManager>().spawnPrefabs.Add(Resources.Load("Other/Nucleo") as GameObject);
         foreach (GameObject i in Resources.LoadAll("Floaters", typeof(GameObject)))//Agrega los prefabs iniciales al NWM
         {
             FindObjectOfType<NetworkManager>().spawnPrefabs.Add(i);
